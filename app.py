b3512b86a0ba9d9591dacde60688ee5766fd244f
randyelorm <randyelorm@yahoo.com>
2022-11-15 16:46:23 +0000

    updated it to text

--- app.py
+++ app.py
@@ -3,20 +3,35 @@ import requests
 from bs4 import BeautifulSoup
 from flask import Flask, request, jsonify
 from flask_cors import CORS, cross_origin
+import requests_html
 
 app = Flask(__name__)
 CORS(app)
 
 
+# @app.route("/", methods=["POST"])
+# def getdata():
+#     url = request.form.get("url")
+#     webpage = requests.get(url).text
+#     soup = BeautifulSoup(webpage, 'html.parser')
+#     allurls = []
+#     for item in soup.find_all('img', {"src": True}):
+#         allurls.append(item['src'])
+#     return allurls
+
+
+# Session stores actions of the user on the page whilst cookie stores it on the computer.
+
 @app.route("/", methods=["POST"])
-def getdata():
-    url = request.form.get("url")
+def search():
+    search_term = request.form.get("search_term")
+    url = f'https://www.google.com/search?q={search_term}&rlz=1C5CHFA_enGH1021GH1021&sxsrf=ALiCzsZxSRkdUWaaI1tweR-MMlOsvqNPHQ:1668530024055&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjjzoXbzrD7AhU3wAIHHZS3B18Q_AUoAnoECAIQBA&biw=1280&bih=913&dpr=2'
     webpage = requests.get(url).text
     soup = BeautifulSoup(webpage, 'html.parser')
-    allurls = []
+    out_results = []
     for item in soup.find_all('img', {"src": True}):
-        allurls.append(item['src'])
-    return allurls
+        out_results.append(item['src'])
+    return out_results
 
 
 if __name__ == "__main__":
