6f4cc95fe966d9968867521fa46e70815577ab5b
randyelorm <randyelorm@yahoo.com>
2023-10-31 15:12:48 +0000

    changes

--- app.py
+++ app.py
@@ -21,12 +21,16 @@ CORS(app)
 
 
 # Session stores actions of the user on the page whilst cookie stores it on the computer.
+# headers were added so it looks like a request from a browser
 
 @app.route("/", methods=["POST"])
 def search():
     search_term = request.form.get("search_term")
     url = f'https://www.google.com/search?q={search_term}&rlz=1C5CHFA_enGH1021GH1021&sxsrf=ALiCzsZxSRkdUWaaI1tweR-MMlOsvqNPHQ:1668530024055&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjjzoXbzrD7AhU3wAIHHZS3B18Q_AUoAnoECAIQBA&biw=1280&bih=913&dpr=2'
-    webpage = requests.get(url).text
+    headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+    }
+    webpage = requests.get(url, headers=headers).text
     soup = BeautifulSoup(webpage, 'html.parser')
     out_results = []
     for item in soup.find_all('img', {"src": True}):
